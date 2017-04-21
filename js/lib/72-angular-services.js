47d42d8832cac1f0a9d66c3b7c2587b607c92f9a
jaros1 <github@gofreerev.com>
2017-04-21 15:19:06 +0200

    added some initial libraries jquery, bootstrap, angularjs, authbahn and a empty angularjs app

---
+++ js/lib/72-angular-services.js
@@ -0,0 +1,12 @@
+angular.module('MoneyNetworkW1')
+
+    .factory('MoneyNetworkW1Service', ['$timeout', '$rootScope', '$window', '$location',
+        function ($timeout, $rootScope, $window, $location) {
+            var service = 'MoneyNetworkW1Service';
+            console.log(service + ' loaded');
+
+            // export MoneyNetworkW1Service
+            return {};
+
+            // end MoneyNetworkW1Service
+        }]);
---
+++ js/lib/79-angular-wallet-ctrl.js
@@ -0,0 +1,11 @@
+angular.module('MoneyNetworkW1')
+
+    .controller('WalletCtrl', ['$window', 'MoneyNetworkW1Service', function ($window, moneyNetworkService) {
+        var self = this;
+        var controller = 'WalletCtrl';
+        console.log(controller + ' loaded');
+
+        // end WalletCtrl
+    }])
+
+;
